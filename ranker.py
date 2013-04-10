e61c084627e1c6985d2c6dfaf14a240bc805bea0
Peter Law <PeterJCLaw@gmail.com>
2013-04-10 18:55:50 +0100

    Add functionality & tests.
    This is cloned from the ranker from Student Robotics 2012:
    * http://srobo.org/cgit/comp/compdis.git/tree/bin/scores.py
    * http://srobo.org/cgit/comp/compdis.git/tree/test/test-scores.py
    with some minor modifications.

---
+++ ranker.py
@@ -0,0 +1,73 @@
+
+def calc_positions(zpoints, dsq_list):
+	"""
+	A function to work out the placings of zones in a game, given the game points.
+	@param zpoints: a dict of zone number to game points.
+	@param dsq_list: a list of zones that should be considered below last.
+	@returns: a dict of position to array of zone numbers.
+	"""
+
+	pos_map = {}
+	points_map = {}
+
+	for z, p in zpoints.iteritems():
+		if z in dsq_list:
+			p = -1
+		if not points_map.has_key(p):
+			points_map[p] = []
+		points_map[p].append(z)
+
+	i = 1
+	for p in sorted(points_map.keys(), reverse = True):
+		pos_map[i] = points_map[p]
+		i += len(points_map[p])
+
+	return pos_map
+
+def calc_ranked_points(pos_map, dsq_list):
+	"""
+	A function to work out the ranked points for each zone, given the rankings within that game.
+	@param pos_map: a dict of position to array of zone numbers.
+	@param dsq_list: a list of zones that shouldn't be awarded ranked points.
+	@returns: a dict of zone number to rank points.
+	"""
+
+	rpoints = {}
+
+	for pos, zones in pos_map.iteritems():
+		# remove any that are dsqaulified
+		# note that we do this before working out the ties, so that any
+		# dsq tie members are removed from contention
+		zones = [ z for z in zones if z not in dsq_list ]
+		if len(zones) == 0:
+			continue
+
+		# max points is 4, add one because pos is 1-indexed
+		points = (4 + 1) - pos
+		# Now that we have the value for this position if it were not a tie,
+		# we need to allow for ties. In case of a tie, the available points
+		# for all the places used are shared by all those thus placed.
+		# Eg: three first places get 3pts each (4+3+2)/3.
+		# Rather than generate a list and average it, it's quicker to just
+		# do some maths using the max value and the length of the list
+		points = points - ( (len(zones) - 1) / 2.0 )
+		for z in zones:
+			rpoints[z] = points
+
+	# those that were dsq get 0
+	for z in dsq_list:
+		rpoints[z] = 0.0
+
+	return rpoints
+
+def get_ranked_points(zpoints, dsq):
+	"""
+	A function to work out the rank points for each zone, given the game points.
+	This is a thin convenience wrapper around `calc_positions` and `calc_rank_points`.
+	@param zpoints: a dict of zone number to game points.
+	@param dsq: a list of zones that shouldn't be awarded ranked points.
+	@returns: a dict of zone number to rank points.
+	"""
+	pos_map = calc_positions(zpoints, dsq)
+	rpoints = calc_ranked_points(pos_map, dsq)
+	return rpoints
---
+++ test/test-ranks.py
@@ -0,0 +1,69 @@
+
+import os.path
+import sys
+
+# Hack the path..
+
+p = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(0, p)
+del p
+
+# External
+import unittest
+
+# Local
+import ranker
+import utils as test_util
+
+simple_data = { '0': 3, '1': 2, '2': 1, '3': 0 }
+simple_pos = { 1: ['0'], 2: ['1'], 3: ['2'], 4: ['3'] }
+simple_points = { '0': 4.0, '1': 3.0, '2': 2.0, '3': 1.0 }
+
+dsq_data = { '0': 3, '1': 2, '2': 1, '3': 0 }
+dsq_dsq = [ '0', '2' ]
+dsq_pos = { 1: ['1'], 2: ['3'], 3: ['0', '2'] }
+dsq_points = { '0': 0.0, '1': 4.0, '2': 0.0, '3': 3.0 }
+
+tie1_data = { '0': 3, '1': 3, '2': 0, '3': 0 }
+tie1_pos = { 1: ['1', '0'], 3: ['3', '2'] }
+tie1_points = { '0': 3.5, '1': 3.5, '2': 1.5, '3': 1.5 }
+
+tie2_data = { '0': 3, '1': 3, '2': 0, '3': 0 }
+tie2_dsq = [ '0', '2' ]
+tie2_pos = { 1: ['1'], 2: ['3'], 3: ['0', '2'] }
+tie2_points = { '0': 0.0, '1': 4.0, '2': 0.0, '3': 3.0 }
+
+class PositionsTests(unittest.TestCase):
+
+	def test_simple(self):
+		pos = ranker.calc_positions(simple_data, [])
+		test_util.assertEqual(simple_pos, pos, "Wrong positions")
+
+	def test_tie(self):
+		pos = ranker.calc_positions(tie1_data, [])
+		test_util.assertEqual(tie1_pos, pos, "Wrong positions")
+
+	def test_dsq(self):
+		pos = ranker.calc_positions(dsq_data, dsq_dsq)
+		test_util.assertEqual(dsq_pos, pos, "Wrong positions")
+
+	def test_dsq_tie(self):
+		pos = ranker.calc_positions(tie2_data, tie2_dsq)
+		test_util.assertEqual(tie2_pos, pos, "Wrong positions")
+
+class RankedPointsTests(unittest.TestCase):
+
+	def test_simple(self):
+		points = ranker.calc_ranked_points(simple_pos, [])
+		test_util.assertEqual(simple_points, points, "Wrong points")
+
+	def test_tie(self):
+		points = ranker.calc_ranked_points(tie1_pos, [])
+		test_util.assertEqual(tie1_points, points, "Wrong points")
+
+	def test_dsq_tie(self):
+		points = ranker.calc_ranked_points(tie2_pos, tie2_dsq)
+		test_util.assertEqual(tie2_points, points, "Wrong points")
+
+if __name__ == '__main__':
+	unittest.main(buffer=True)
