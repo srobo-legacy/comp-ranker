587e589f978a10a6efdeae704348f025b1ebafe8
Peter Law <PeterJCLaw@gmail.com>
2014-07-27 21:15:55 +0100

    Remove my test utils. Use Nose instead.
    Nose is pretty much always available now, and I just didn't know
    about it when I made the utils. When combined with the -d option,
    it provides just as much info as the utils, and avoids using a
    project specific framework.
    
    Change-Id: I568ab9988cd68229c20ca47924ebfeb262c18a88

--- test/test-ranks.py
+++ test/test-ranks.py
@@ -13,7 +13,6 @@ import unittest
 
 # Local
 import ranker
-import utils as test_util
 
 simple_data = { '0': 3, '1': 2, '2': 1, '3': 0 }
 simple_pos = { 1: set(['0']), 2: set(['1']), 3: set(['2']), 4: set(['3']) }
@@ -37,33 +36,33 @@ class PositionsTests(unittest.TestCase):
 
     def test_simple(self):
         pos = ranker.calc_positions(simple_data, [])
-        test_util.assertEqual(simple_pos, pos, "Wrong positions")
+        assert simple_pos == pos, "Wrong positions"
 
     def test_tie(self):
         pos = ranker.calc_positions(tie1_data, [])
-        test_util.assertEqual(tie1_pos, pos, "Wrong positions")
+        assert tie1_pos == pos, "Wrong positions"
 
     def test_dsq(self):
         pos = ranker.calc_positions(dsq_data, dsq_dsq)
-        test_util.assertEqual(dsq_pos, pos, "Wrong positions")
+        assert dsq_pos == pos, "Wrong positions"
 
     def test_dsq_tie(self):
         pos = ranker.calc_positions(tie2_data, tie2_dsq)
-        test_util.assertEqual(tie2_pos, pos, "Wrong positions")
+        assert tie2_pos == pos, "Wrong positions"
 
 class RankedPointsTests(unittest.TestCase):
 
     def test_simple(self):
         points = ranker.calc_ranked_points(simple_pos, [])
-        test_util.assertEqual(simple_points, points, "Wrong points")
+        assert simple_points == points, "Wrong points"
 
     def test_tie(self):
         points = ranker.calc_ranked_points(tie1_pos, [])
-        test_util.assertEqual(tie1_points, points, "Wrong points")
+        assert tie1_points == points, "Wrong points"
 
     def test_dsq_tie(self):
         points = ranker.calc_ranked_points(tie2_pos, tie2_dsq)
-        test_util.assertEqual(tie2_points, points, "Wrong points")
+        assert tie2_points == points, "Wrong points"
 
 if __name__ == '__main__':
     unittest.main(buffer=True)
