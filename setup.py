bec63f25c268750593526d71474aed4ea4bdcc99
Alistair Lynn <arplynn@gmail.com>
2014-08-03 12:11:56 +0100

    Add a setup.py to ranker
    This means it can be installed via pip.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ranker',
+      maintainer='Peter Law',
+      maintainer_email='plaw@studentrobotics.org',
+      version='1.0.0',
+      url='https://www.studentrobotics.org/cgit/comp/ranker.git/',
+      packages=['ranker'])
+
