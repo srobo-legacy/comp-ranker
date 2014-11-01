de7fb1c018464d36fc87a8c5b93d97e30ec316d0
Alistair Lynn <arplynn@gmail.com>
2014-11-01 09:04:02 +0000

    Add setuptools-style setup.py
    Patch Set #2: Fix Peter's email address, use sr- in the package name
    
    Change-Id: Ie587993b2e92fc25eca8c57bfc7d27c34500ce5d

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    description = f.read()
+
+setup(name = "sr-ranker",
+      version = "1.0",
+      packages = find_packages(),
+      description = description,
+      author = "Peter Law",
+      author_email = "PeterJCLaw@gmail.com",
+      install_requires = ['nose >=1.3, <2'],
+      zip_safe = True
+      )
+
